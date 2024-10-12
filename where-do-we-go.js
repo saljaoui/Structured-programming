a4dc10c66de377d774cb0dc991bafec965bd0a06
saljaoui <soufian2022ff@gmail.com>
2024-10-12 21:08:23 +0100

    update

---
+++ where-do-we-go.js
@@ -0,0 +1,9 @@
+import { places } from "./where-do-we-go.data.js";
+
+export const explore = () => {
+
+    let section = document.createElement('section')
+    document.body.appendChild(section)
+    console.log(places);
+
+}
